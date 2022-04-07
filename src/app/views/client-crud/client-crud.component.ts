470b31ceda012c0c68b9d96c00685d29077de25b
Daniel Haag <danni.haag91@gmail.com>
2022-04-06 23:33:22 -0300

    serviço de manipulação do header injetado no componente de clientes

--- src/app/views/client-crud/client-crud.component.ts
+++ src/app/views/client-crud/client-crud.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HeaderService } from 'src/app/components/template/header/header.service';
 
 @Component({
   selector: 'app-client-crud',
@@ -8,7 +9,13 @@ import { Router } from '@angular/router';
 })
 export class ClientCrudComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private headerService: HeaderService) {
+    headerService.headerData = {
+      title: "Cadastro de Clientes",
+      icon: "person",
+      routeUrl: "/"
+    }
+   }
 
   ngOnInit(): void {
   }
